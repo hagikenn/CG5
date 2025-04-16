c4c9a940e7522092abf6cef7e59439870db047bf
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-04-16 11:55:26 +0900

    青い画面表示完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include <kamataEngine.h>
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -7,12 +8,21 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//エンジンの初期化
 	KamataEngine::Initialize(L"LE3D_11_ハギワラ_ケンタ");
 
+	//DirectXCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
 	//メインループ
 	while (true) {
 		// エンジンの更新
 		if (KamataEngine::Update()) {
 			break;
 		}
+
+		//描画開始
+		dxCommon->PreDraw();
+
+		//描画終了
+		dxCommon->PostDraw();
 	}
 
 	// エンジンの終了処理
