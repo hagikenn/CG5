204478822110d9ffe3f3964d907dcb49867edbbd
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-05-14 12:32:46 +0900

    00_02確認課題完了

--- DirectXGame/Resources/shaders/TestVS.hlsl
+++ DirectXGame/Resources/shaders/TestVS.hlsl
@@ -7,8 +7,9 @@ struct VertexShaderInput
     float4 position : POSITION0;
 };
 
-VertexShaderOutput main(VertexShaderInput input){
+VertexShaderOutput main(VertexShaderInput input)
+{
     VertexShaderOutput output;
     output.position = input.position;
-	return output;
+    return output;
 }
