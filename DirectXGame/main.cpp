543c26e799480edd209d089f402dfaa610ce5a17
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-06-10 12:27:08 +0900

    00_05確認課題完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -4,6 +4,7 @@
 #include<cassert>
 #include"RootSignature.h"
 #include"PipelineState.h"
+#include"VertexBuffer.h"
 using namespace KamataEngine;
 
 void SetupPipelineState(PipelineState& pipelineState, RootSignature& rs, Shader& vs, Shader& ps) {
@@ -89,53 +90,20 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	PipelineState PipelineState;
 	SetupPipelineState(PipelineState, rs, vs, ps);
 	
-
-	// VertexResourceの生成
-	// --------------------
-	// 頂点リソース用のヒープの設定
-	D3D12_HEAP_PROPERTIES uploadHeapProperties{};
-	uploadHeapProperties.Type = D3D12_HEAP_TYPE_UPLOAD; // CPUから書き込むヒープ
-
-	// 頂点リソースの設定
-	D3D12_RESOURCE_DESC vertexResourceDesc{};
-	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER; // バッファ
-	vertexResourceDesc.Width = sizeof(Vector4) * 3;                 // リソースのサイズ。今回はVector4を3頂点分
-	// バッファの場合はこれらは1に決まり
-	vertexResourceDesc.Height = 1;
-	vertexResourceDesc.DepthOrArraySize = 1;
-	vertexResourceDesc.MipLevels = 1;
-	vertexResourceDesc.SampleDesc.Count = 1;
-	// バッファの場合はこれに決まり
-	vertexResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-
-	// 実際に頂点リソースを生成する
-	ID3D12Resource* vertexResource = nullptr;
-	HRESULT hr =dxCommon->GetDevice()->CreateCommittedResource(&uploadHeapProperties, D3D12_HEAP_FLAG_NONE, &vertexResourceDesc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&vertexResource));
-	assert(SUCCEEDED(hr)); // うまくいかなかったときは起動できない
-
-	// VertexBufferViewを作成する
-	// --------------------
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{};
-
-	// リソースの先頭アドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-
-	// 使用するリソースのサイズは頂点3つ分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(Vector4) * 3;
-
-	// 1つの頂点のサイズ
-	vertexBufferView.StrideInBytes = sizeof(Vector4);
-
+	//VertexBuffer(VertexResource,VertexResourceView)の生成
+	VertexBuffer vb;
+	vb.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	
 	// 頂点リソースにデータを書き込む
 	// --------------------
 	Vector4* vertexData = nullptr;
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
 	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f}; // 左下
 	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f};   // 上
 	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f};  // 右下
 
 	// 頂点リソースのマップを解除する
-	vertexResource->Unmap(0, nullptr);
+	//vb.Get()->Unmap(0, nullptr);
 	
 	//メインループ
 	while (true) {
@@ -152,7 +120,7 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		// コマンドを積む
 		commandList->SetGraphicsRootSignature(rs.Get());     // RootSignatureの設定
 		commandList->SetPipelineState(PipelineState.Get());     // PSOの設定
-		commandList->IASetVertexBuffers(0, 1, &vertexBufferView); // VBVの設定
+		commandList->IASetVertexBuffers(0, 1,vb.GetView()); // VBVの設定
 
 		// トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -164,9 +132,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		dxCommon->PostDraw();
 	}
 
-	// 解放処理
-	vertexResource->Release();
-	//graphicsPipelineState->Release();
 	
 	// エンジンの終了処理
 	KamataEngine::Finalize();
